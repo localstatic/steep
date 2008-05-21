5bd63874b587140a32b3ed9785be00cef795f266
mt <mt@25aa0472-a5d5-0310-b9f0-db4f2a0ab381>
2008-05-21 05:20:45 +0000

    Modified to use glade for UI instead of building the widgets explicitly in Python code.

--- steep.py
+++ steep.py
@@ -3,6 +3,7 @@
 import pygtk
 pygtk.require('2.0')
 import gtk
+import gtk.glade
 import gobject
 
 #
@@ -15,110 +16,70 @@ import time
 
 import timer
 
-class steeptimer:
+class SteepTimer:
     def __init__(self):
         gobject.threads_init()
-        self._ticks_remaining = 5
+        self._ticks = 5
+        self._remaining = 0
         self._timer = timer.Timer(1, self.tick)
-        self.build_ui()
 
-    def start(self, widget = None, data = None):
-        self._ticks_remaining = 5
+        # set up UI via Glade
+        
+        wtree = gtk.glade.XML('steep.glade')
+        wtree.signal_autoconnect(self)
+        
+        for w in wtree.get_widget_prefix(''):
+            name = w.get_name()
+            assert not hasattr(self, name)
+            setattr(self, name, w)
+            
+        self.wnd_main.show()
+
+        #
+        
+        self.on_btn_reset_clicked()
+        
+    def on_btn_start_clicked(self, widget = None, data = None):
+        self._remaining = self._ticks
         print "Starting."
-        self.lblTimerTime.set_text(str(self._ticks_remaining))
+        self.update_timer_display()
         self._timer.start()
 
-    def stop(self, widget = None, data = None):
+    def on_btn_stop_clicked(self, widget = None, data = None):
         print "Stopping."
         self._timer.stop()
-        self.lblTimerTime.set_text(str(self._ticks_remaining))
+        self.update_timer_display()
 
-    def reset(self, widget = None, data = None):
+    def on_btn_reset_clicked(self, widget = None, data = None):
         print "Resetting."
-        self._ticks_remaining = 5
-        self.lblTimerTime.set_text(str(self._ticks_remaining))
+        self.on_btn_stop_clicked()
+        self._remaining = self._ticks
+        self.update_timer_display()
+        
+    def update_timer_display(self):
+        self.lbl_remaining.set_text(str(self._remaining))
         
     def tick(self):
         print "tick."
-        self._ticks_remaining = self._ticks_remaining - 1
-        self.lblTimerTime.set_text(str(self._ticks_remaining))
+        self._remaining = self._remaining - 1
+        self.lbl_remaining.set_text(str(self._remaining))
         
-        if self._ticks_remaining <= 0:
+        if self._remaining <= 0:
             print "done."
-            self.stop()
+            self.on_btn_stop_clicked()
         
-    def main(self):
-        gtk.main()
-
-    def say_something(self, widget, data):
-        print "%s" % data
-
-    def delete_event(self, widget, event, data=None):
+    def on_wnd_main_delete_event(self, widget, event, data=None):
         print "delete event"
         return False
 
-    def destroy(self, widget, data=None):
+    def on_wnd_main_destroy(self, widget, data=None):
         gtk.main_quit()
 
-    def build_ui(self):
-        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        self.window.connect("delete_event", self.delete_event)
-        self.window.connect("destroy", self.destroy)
-
-        self.label1 = gtk.Label("Placeholder Label")
-        self.separator1 = gtk.HSeparator()
-
-        self.btnStart = gtk.Button("Start")
-        self.btnStart.connect("clicked", self.start, "Starting timer.")
-        
-        self.btnStop = gtk.Button("Stop")
-        self.btnStop.connect("clicked", self.stop, "Stopping timer.")
-
-        self.btnReset = gtk.Button("Reset")
-        self.btnReset.connect("clicked", self.reset, "Resetting timer.")
-
-        self.btnQuit = gtk.Button("Quit")
-        self.btnQuit.connect("clicked", self.say_something, "Quitting.")
-        self.btnQuit.connect_object("clicked", gtk.Widget.destroy, self.window)
-
-        self.boxTimerUI = gtk.HBox(True, 0)
-    
-        self.boxTimerButtons = gtk.VBox(True, 0)
-        self.boxTimerButtons.pack_start(self.btnStart, False)
-        self.boxTimerButtons.pack_start(self.btnStop, False)
-        self.boxTimerButtons.pack_start(self.btnReset, False)
-        
-        self.lblTimerTime = gtk.Label("0:00")    
-        
-        self.boxTimerUI.pack_start(self.lblTimerTime, False)
-        self.boxTimerUI.pack_start(self.boxTimerButtons, False, False)
-
-        self.vboxMain = gtk.VBox(True, 0)
-        self.vboxMain.pack_start(self.boxTimerUI, True, True)
-        self.vboxMain.pack_start(self.label1, True, False)
-        self.vboxMain.pack_start(self.separator1, False, True)
-        self.vboxMain.pack_end(self.btnQuit, True, False)
-
-        self.window.add(self.vboxMain)
-
-        self.label1.show()
-        self.separator1.show()
-
-        self.btnStart.show()
-        self.btnStop.show()
-        self.btnReset.show()
-
-        self.btnQuit.show()
-        
-        self.lblTimerTime.show()
-        self.boxTimerButtons.show()
-        self.boxTimerUI.show()
-        self.vboxMain.show()
-
-        self.window.show()
+    def main(self):
+        gtk.main()
 
 if __name__ == "__main__":
-    obj = steeptimer()
+    obj = SteepTimer()
     obj.main()
 
 
