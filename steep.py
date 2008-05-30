e28018432c040fe619e5663fa860af9303a2bca7
mt <mt@25aa0472-a5d5-0310-b9f0-db4f2a0ab381>
2008-05-30 04:04:33 +0000

    * Added message dialog notification that the timer has elapsed. * Use logging module for printing debug stuff instead of printing directly.

--- steep.py
+++ steep.py
@@ -6,6 +6,7 @@ import gobject
 
 #
 
+import logging
 import string
 import thread
 import time
@@ -14,8 +15,9 @@ import time
 
 import timer
 
-class SteepTimer:
+class SteepMain:
     def __init__(self):
+        logging.basicConfig(format="%(asctime)s %(levelname)s %(message)s", level=logging.DEBUG)
         gobject.threads_init()
         self._ticks = 0
         self._remaining = 0
@@ -35,27 +37,32 @@ class SteepTimer:
 
         #
 
-        self.on_btn_reset_clicked()
+        self.on_tea_selected()
+        self.reset_timer()
         
     def on_btn_start_clicked(self, widget = None, data = None):
-        self.init_timer(self._ticks)
-
-        print "Starting."
+        self.on_tea_selected()
+        logging.debug("Starting.");
         self.update_timer_display()
         self._timer.start()
 
     def on_btn_stop_clicked(self, widget = None, data = None):
-        print "Stopping."
+        if not self._timer.is_running():
+            return
+        
+        logging.debug("Stopping.")
         self._timer.stop()
         self.update_timer_display()
 
-    def on_btn_reset_clicked(self, widget = None, data = None):
-        print "Resetting."
+    def reset_timer(self):
+        logging.debug("Resetting.")
         self.on_btn_stop_clicked()
         self._remaining = self._ticks
         self.update_timer_display()
         
-    def on_tea_selected(self, widget, data = None):
+    def on_tea_selected(self, widget = None, data = None):
+        self.on_btn_stop_clicked()
+
         if self.rb_blacktea.get_active():
             seconds = 5 * 60
         elif self.rb_greentea.get_active():
@@ -63,11 +70,9 @@ class SteepTimer:
         elif self.rb_herbaltea.get_active():
             seconds = 7 * 60
         
-        self.on_btn_stop_clicked()
         self.init_timer(seconds)
     
     def on_wnd_main_delete_event(self, widget, event, data=None):
-        print "delete event"
         return False
 
     def on_wnd_main_destroy(self, widget, data=None):
@@ -76,31 +81,37 @@ class SteepTimer:
     def init_timer(self, seconds):
         self._ticks = seconds
         self._remaining = self._ticks
-        
         self.pb_progressbar.set_fraction(0)
         self.update_timer_display()
         
     def update_timer_display(self):
         formatted_time_string = '%d:%02d' % (self._remaining / 60, self._remaining % 60)
-        self.lbl_remaining.set_text(formatted_time_string)
+        self.pb_progressbar.set_text(formatted_time_string)
         
+        if 0 != self._ticks:
+            self.pb_progressbar.set_fraction(float(self._remaining) / self._ticks)
+
     def tick(self):
-        print "tick."
+        logging.debug("tick (" + str(self._remaining) + ").")
         self._remaining = self._remaining - 1
         self.update_timer_display()
-        self.pb_progressbar.set_fraction(1 - float(self._remaining) / self._ticks)
-        self.pb_progressbar.set_text(self.lbl_remaining.get_text())
+
         if self._remaining <= 0:
-            print "done."
-            #msgdlg = gtk.MessageDialog(self.wnd_main, gtk.DIALOG_MODAL, gtk.MESSAGE_INFO, gtk.BUTTONS_OK, 'Tea is ready.')
-            #msgdlg.run()
+            logging.debug("done.")
             self.on_btn_stop_clicked()
+            gobject.idle_add(self.timer_elapsed)
+            
+    def timer_elapsed(self):
+        dlg = gtk.MessageDialog(self.wnd_main, gtk.DIALOG_MODAL, gtk.MESSAGE_INFO, gtk.BUTTONS_OK, 'Tea is ready.')
+        dlg.run()
+        dlg.destroy()
+        
         
     def main(self):
         gtk.main()
 
 if __name__ == "__main__":
-    obj = SteepTimer()
-    obj.main()
+    obj = SteepMain()
+    gtk.main()
 
 
