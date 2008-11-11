26418321420a96b89eb65278f435200aa1d674f8
mt <mt@25aa0472-a5d5-0310-b9f0-db4f2a0ab381>
2008-11-11 07:43:02 +0000

    * Read "finished" message from config file instead of hard coding it. * Read timers from config file (steep.conf) instead of hard coding them. * Implemented TreeView for displaying/selecting timers instead of radio buttons.

--- steep.py
+++ steep.py
@@ -7,19 +7,25 @@ import logging
 import string
 import thread
 import time
+import ConfigParser
 
 import timer
 
 class Steep:
     def __init__(self):
         logging.basicConfig(format="%(asctime)s %(levelname)s %(message)s", level=logging.DEBUG)
+
+        # Read config & do any relevant setup
+        self._config_values = self.read_config()
+        logging.debug(self._config_values)
+        
+        #
         gobject.threads_init()
         self._seconds = 0
         self._remaining = 0
         self._timer = timer.Timer(1, self.tick)
 
         # set up UI via Glade
-        
         wtree = gtk.glade.XML('steep.glade', 'wnd_main')
         wtree.signal_autoconnect(self)
         
@@ -27,14 +33,55 @@ class Steep:
             name = w.get_name()
             assert not hasattr(self, name)
             setattr(self, name, w)
+      
+        # set up tree view with timers from the config file
+        cell = gtk.CellRendererText()
+        col = gtk.TreeViewColumn('Name', cell)
+        col.add_attribute(cell, 'text', 0)
+        self.tv_timers.append_column(col)
+        
+        cell = gtk.CellRendererText()
+        col = gtk.TreeViewColumn('Duration', cell)
+        col.set_cell_data_func(cell, self.render_timer_duration, None)
+        self.tv_timers.append_column(col)
+        
+        store = gtk.ListStore(gobject.TYPE_STRING, gobject.TYPE_INT)
+
+        for t in self._config_values['timers']:
+            timer_values = self._config_values['timers'][t]
+            store.append((timer_values['label'], int(timer_values['duration'])))
             
-        self.wnd_main.show()
+        self.tv_timers.set_model(store)
+        self.tv_timers.columns_autosize()
 
         #
-
-        self.on_tea_selected()
+        self.wnd_main.show()
         self.reset_timer()
+    
+    def render_timer_duration(self, column, cell, model, iter, user_data):
+        cell.set_property('text', self.get_seconds_time_display_string(model.get_value(iter, 1)))
+    
+    def read_config(self):
+        config = ConfigParser.ConfigParser()
+        config.read('steep.conf')
+
+        config_values = dict(config.items('options'))
+        sections = config.sections()
         
+        if len(sections) > 0:
+            config_values['timers'] = {}
+            for sect in sections:
+                if sect.startswith('timer.'):
+                    items = config.items(sect)
+                    sect_items = {}
+                    
+                    for name, value in items:
+                        sect_items[name] = value
+                    
+                    config_values['timers'][sect.replace('timer.', '')] = sect_items 
+                
+        return config_values
+            
     def on_btn_startstop_clicked(self, widget = None, data = None):
         if self._timer.is_running():
             self.stop_timer()
@@ -44,17 +91,33 @@ class Steep:
     def on_btn_exit_clicked(self, widget = None, data = None):
         gtk.main_quit()
         
-    def on_tea_selected(self, widget = None, data = None):
+    def on_tv_row_activated(self, treeview, view_column, unused):
+        model, iter = treeview.get_selection().get_selected()
+        
+        seconds = model.get_value(iter, 1)
+        
         self.stop_timer()
-        
-        if self.rb_blacktea.get_active():
-            seconds = 5 * 60
-        elif self.rb_greentea.get_active():
-            seconds = 3 * 60
-        elif self.rb_herbaltea.get_active():
-            seconds = 7 * 60
-        
         self.init_timer(seconds)
+
+        logging.debug('timer row activated: ' + str(seconds) + ' seconds')
+        
+        self.start_timer()
+
+    def on_tv_timers_changed(self, treeview = None):
+        if None == treeview:
+            treeview = self.tv_timers
+        
+        model, iter = treeview.get_selection().get_selected()
+        
+        if None == iter:
+            iter = model.get_iter_first()
+        
+        seconds = model.get_value(iter, 1)
+        
+        self.stop_timer()
+        self.init_timer(seconds)
+        
+        logging.debug('timer selection changed: ' + str(seconds) + ' seconds')
     
     def on_wnd_main_delete_event(self, widget, event, data=None):
         return False
@@ -70,7 +133,7 @@ class Steep:
         
     def start_timer(self):
         if 0 == self._remaining:
-            self.on_tea_selected()
+            self.on_tv_timers_changed()
             
         logging.debug("Starting.");
         self._started_at = time.time()
@@ -95,11 +158,14 @@ class Steep:
         self._remaining = self._seconds
         self.update_timer_display()
         
+    def get_seconds_time_display_string(self, seconds):
+        return '%d:%02d' % (seconds / 60, seconds % 60)
+        
     def update_timer_display(self):
         remaining = int(self._remaining)
         elapsed_seconds = self._seconds - remaining
-        total_time_string = '%d:%02d' % (elapsed_seconds / 60, elapsed_seconds % 60)
-        remaining_time_string = '%d:%02d' % (remaining / 60, remaining % 60)
+        total_time_string = self.get_seconds_time_display_string(elapsed_seconds)
+        remaining_time_string = self.get_seconds_time_display_string(remaining)
         self.pb_progressbar.set_text(total_time_string + ' / -' + remaining_time_string)
         
         if 0 != self._seconds and 0 < self._remaining:
@@ -120,7 +186,8 @@ class Steep:
             gobject.idle_add(self.timer_elapsed)
             
     def timer_elapsed(self):
-        dlg = gtk.MessageDialog(self.wnd_main, gtk.DIALOG_MODAL, gtk.MESSAGE_INFO, gtk.BUTTONS_OK, 'Tea is ready.')
+        finished_message = self._config_values['finished_message'] or 'Done.'
+        dlg = gtk.MessageDialog(self.wnd_main, gtk.DIALOG_MODAL, gtk.MESSAGE_INFO, gtk.BUTTONS_OK, finished_message)
         dlg.run()
         dlg.destroy()
         
